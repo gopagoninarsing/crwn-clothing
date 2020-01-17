3661ceb3c18c0d05c3ec51da50e9fbbed5449d3a
Narsing Gopagoni <narsing@azrisolutions.com>
2020-01-17 11:04:29 +0530

    signin redirect to homw

--- src/App.js
+++ src/App.js
@@ -47,8 +47,12 @@ class App extends React.Component {
 
             <Route exact
                   path='/signin'
-                  render = {() => this.props.currentUser? <Redirect to='/' /> : <SignInAndSignUpPage />}
-                  component={SignInAndSignUpPage} />
+                  render = {() => 
+                    this.props.currentUser?
+                     (<Redirect to='/' />) 
+                     : (<SignInAndSignUpPage />)
+                    }
+             />
           </Switch>
       </div>
     );
