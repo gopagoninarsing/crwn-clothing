fcf33ff09bf9214cd3aa9f5b65bdc5d34d55ad42
Narsing Gopagoni <narsing@azrisolutions.com>
2020-04-17 14:29:08 +0530

    udemy #130

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,21 +1,12 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
-import { selectCollections } from '../../redux/shop/shop.selectors';
-import CollectionPreview from '../../components/collectionPreview/collectionPreview.component';
 
 const ShopPage = ({ collections }) => (
     <div className='shop-page'>
-        { collections.map(({id, ...otherCollectionItems }) => (
-                <CollectionPreview key={id} { ...otherCollectionItems} />
-            ))
-        }
+      <CollectionsOverview />
     </div>
 );
  
-const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
-});
 
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
